64b5aeb25a3093cbca28f48052b4e6d50ce328e3
Brian Huang <brian800912@gmail.com>
2019-08-22 11:57:47 -0700

    complete register.js and header.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header } from './Header';
+import { Main } from './Main';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
