2112e6a89d3175777c6fad351fb65a4b79a394df
Brian Huang <brian800912@gmail.com>
2019-08-22 19:43:06 -0700

    complete createPostButtom.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'APi';
+export const API_ROOT = 'http://35.184.169.207:8080';
+//export const API_ROOT = 'API_ROOT';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS = {
     enableHighAccuracy: true,
